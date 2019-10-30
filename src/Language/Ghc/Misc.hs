928008cbba6de2cb1b31f7171fbbfe7b8c619621
DavidMazarro <davidmazarro98@gmail.com>
2019-10-30 13:10:14 +0100

    Changed printing for Core constructors

--- src/Language/Ghc/Misc.hs
+++ src/Language/Ghc/Misc.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Language.Ghc.Misc where 
 
+import BasicTypes
+import DataCon
 import Outputable
 import TyCoRep 
 import CoreSyn 
@@ -14,23 +17,29 @@ import Var
 import Literal
 
 
-instance Show AltCon where 
-  show = showSDocUnsafe . ppr 
-
+deriving instance Show AltCon
 
 instance Show Type where 
-    show = showSDocUnsafe . ppr 
-instance Show CoreBind where 
-  show = showSDocUnsafe . ppr 
+    show = showSDocUnsafe . ppr
+           
+deriving instance Show CoreBind
+
+instance Show DataCon where
+  show = showSDocUnsafe . ppr
+
+    
 instance Hashable Var where 
   hashWithSalt _ = getKey . getUnique
 
+deriving instance Show Literal
+deriving instance Show LitNumType
+deriving instance Show FunctionOrData
 
-instance Show Literal where 
-  show x = showSDocUnsafe (ppr x)
-
-instance Show CoreExpr where 
-  show x = showSDocUnsafe (ppr x)
-  
-instance Show Var where 
-  show x = showSDocUnsafe (ppr x)
+deriving instance Show CoreExpr
+instance Show (Tickish Id) where
+    show = showSDocUnsafe . ppr
+instance Show Coercion where
+    show = showSDocUnsafe . ppr
+         
+instance Show Var where
+    show x = showSDocUnsafe (ppr x)
