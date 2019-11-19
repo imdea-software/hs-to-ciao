3e42f1ec89c4a56284a8e6f5b06f0cd09bc63040
DavidMazarro <davidmazarro98@gmail.com>
2019-11-19 11:07:07 +0100

    Added compose function to library

--- lib/ciao_prelude.pl
+++ lib/ciao_prelude.pl
@@ -2,3 +2,9 @@
 
 :- meta_predicate bool(goal,?).
 bool(X,T) :- (X -> T=true ; T=false).
+
+:- meta_predicate flip(pred(3),?,?,?).
+flip(F,X,Y) := ~F(Y,X).
+
+:- meta_predicate compose(pred(2),pred(2),?,?).
+compose(F, G, X) := ~F(~G(X)).
