3744990b27dc5b485281d660819e722a57599685
David Mazarro <davidmazarro98@gmail.com>
2019-12-18 12:01:44 +0100

    Example now reflects new variable syntax

--- manual_examples/Foldl.pl
+++ manual_examples/Foldl.pl
@@ -2,4 +2,4 @@
 
 :- meta_predicate foldl(pred(3),?,?,?).
 foldl(F, Base, []) := Base.
-foldl(F, Base, .(X,XS)) := ~foldl(F, ~F(Base, X), XS).
+foldl(F, Base, .(X,Xs)) := ~foldl(F, ~F(Base, X), Xs).
