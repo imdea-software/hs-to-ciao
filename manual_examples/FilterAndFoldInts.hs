4bde13254d2724001eae378113dde8aedce55169
DavidMazarro <davidmazarro98@gmail.com>
2020-01-16 16:57:32 +0100

    Modified example to use only Int version of foldl

--- manual_examples/FilterAndFoldInts.hs
+++ manual_examples/FilterAndFoldInts.hs
@@ -1,6 +1,14 @@
 module FilterAndFoldInts where
 
+import Prelude hiding (foldl)
+
+foldl :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+foldl _ b []     = b
+foldl f b (x:xs) = x `f` foldl f b xs
+    
 -- Testing for point-free style in this example
 filterAndFoldInts :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
-filterAndFoldInts filt f base = (foldl f base) . (filter filt)
+filterAndFoldInts filt f base x = (foldl f base) . (filter filt) $ x
 
+
+                                
