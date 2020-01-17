2dc15bb20f875993f64649bd58f4b4d988930a05
David Munuera Mazarro <david.mmazarro@alumnos.upm.es>
2020-01-17 13:58:57 +0100

    Changed examples so they're not type polymorhpic

--- manual_examples/FilterAndFoldInts.hs
+++ manual_examples/FilterAndFoldInts.hs
@@ -1,11 +1,20 @@
 module FilterAndFoldInts where
 
-import Prelude hiding (foldl)
+import Prelude hiding (foldl, (.), filter)
 
 foldl :: (Int -> Int -> Int) -> Int -> [Int] -> Int
 foldl _ b []     = b
 foldl f b (x:xs) = x `f` foldl f b xs
-    
+
+(.) :: ([Int] -> Int) -> ([Int] -> [Int]) -> [Int] -> Int
+(.) f g = \x -> f (g x)
+
+filter :: (Int -> Bool) -> [Int] -> [Int]
+filter _ [] = []
+filter pred (x:xs) = if pred x then x : filter pred xs else filter pred xs
+  -- | pred x = x : filter pred xs
+  -- | otherwise = filter pred xs
+          
 -- Testing for point-free style in this example
 filterAndFoldInts :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
 filterAndFoldInts filt f base x = (foldl f base) . (filter filt) $ x
--- manual_examples/MapReduce.hs
+++ manual_examples/MapReduce.hs
@@ -1,4 +1,17 @@
 module MapReduce where
 
+import Prelude hiding (foldl, (.), map)
+
+foldl :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+foldl _ b []     = b
+foldl f b (x:xs) = x `f` foldl f b xs
+
+(.) :: ([Int] -> Int) -> ([Int] -> [Int]) -> [Int] -> Int
+(.) f g = \x -> f (g x)
+
+map :: (Int -> Int) -> [Int] -> [Int]
+map _ [] = []
+map f (x:xs) = (f x) : xs
+
 mapreduce :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
-mapreduce f combinator base = (foldl combinator base) . (map f)
+mapreduce f combinator base x = ((foldl combinator base) . (map f)) x
