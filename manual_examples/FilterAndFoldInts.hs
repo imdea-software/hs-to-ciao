e1f19a45e9dd9a75bf2a985494b1752cf396bf24
DavidMazarro <davidmazarro98@gmail.com>
2020-01-30 11:31:36 +0100

    Slight changes to FilterAndFoldInts example syntax

--- manual_examples/FilterAndFoldInts.hs
+++ manual_examples/FilterAndFoldInts.hs
@@ -11,13 +11,13 @@ foldl f b (x:xs) = x `f` foldl f b xs
 
 filter :: (Int -> Bool) -> [Int] -> [Int]
 filter _ [] = []
-filter pred (x:xs) = if pred x then x : filter pred xs else filter pred xs
+filter pred (x:xs) = if pred x then x : (filter pred xs) else filter pred xs
   -- | pred x = x : filter pred xs
   -- | otherwise = filter pred xs
           
 -- Testing for point-free style in this example
 filterAndFoldInts :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
-filterAndFoldInts filt f base x = (foldl f base) . (filter filt) $ x
+filterAndFoldInts filt f base x = ((foldl f base) . (filter filt)) x
 
 
                                 
