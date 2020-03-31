49303ac1e866af8d30a61ca2dcce6ca04d3fe491
DavidMazarro <davidmazarro98@gmail.com>
2020-03-31 11:26:18 +0200

    Simplified examples

--- manual_examples/MapReduce.hs
+++ manual_examples/MapReduce.hs
@@ -1,17 +1,4 @@
 module MapReduce where
 
-import Prelude hiding (foldl, (.), map)
-
-foldl :: (Int -> Int -> Int) -> Int -> [Int] -> Int
-foldl _ b []     = b
-foldl f b (x:xs) = x `f` foldl f b xs
-
-(.) :: ([Int] -> Int) -> ([Int] -> [Int]) -> [Int] -> Int
-(.) f g = \x -> f (g x)
-
-map :: (Int -> Int) -> [Int] -> [Int]
-map _ [] = []
-map f (x:xs) = (f x) : map f xs
-
 mapreduce :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
 mapreduce f combinator base x = ((foldl combinator base) . (map f)) x
--- manual_examples/MergeSort.hs
+++ manual_examples/MergeSort.hs
@@ -25,7 +25,8 @@ mergeSort merge xs
               half = length xs `div` 2
 
 merge :: [Int] -> [Int] -> [Int]
-merge [] ys         = ys
-merge xs []         = xs
-merge (x:xs) (y:ys) | x < y     = x:merge xs (y:ys)
-                    | otherwise = y:merge (x:xs) ys
+merge = undefined
+-- merge [] ys         = ys
+-- merge xs []         = xs
+-- merge (x:xs) (y:ys) | x < y     = x:merge xs (y:ys)
+--                     | otherwise = y:merge (x:xs) ys
