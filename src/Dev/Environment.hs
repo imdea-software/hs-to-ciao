aa6bd9c11be042721d5fc126bd04319f99a2215a
DavidMazarro <davidmazarro98@gmail.com>
2020-04-21 12:16:54 +0200

    Added function to update the bound IDs in the environment

--- src/Dev/Environment.hs
+++ src/Dev/Environment.hs
@@ -15,5 +15,10 @@ data Environment = Environment {
 
 type Env = State Environment
 
+addBoundId :: Id -> Env ()
+addBoundId newId = do
+  env <- get
+  put $ env { boundIds = newId:(boundIds env) }
+    
 getTargetName :: Target -> String
 getTargetName = showSDocUnsafe . pprTargetId . targetId
