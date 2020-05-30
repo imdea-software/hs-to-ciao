f33f56f8181d0a698d29c289548fc326a246bb4a
DavidMazarro <davidmazarro98@gmail.com>
2020-05-30 19:25:23 +0200

    Removed unnecessary modules

--- src/Language/Ciao/Misc.hs
+++
@@ -1,6 +0,0 @@
-module Language.Ciao.Misc where 
-
-import Debug.Trace hiding (traceShow)
-
-traceShow :: Show a => String -> a -> a 
-traceShow msg t = trace ("\n\nTRACE [" ++ msg ++ "]" ++ show t ) t
