f337126935759bf4c40347810fefddf9d69d54ed
DavidMazarro <davidmazarro98@gmail.com>
2020-06-07 20:45:32 +0200

    Now pretty-printer takes the path to the plugin directory

--- src/PrettyPrinters/GeneralPrinter.hs
+++ src/PrettyPrinters/GeneralPrinter.hs
@@ -4,19 +4,22 @@ import PrettyPrinters.AnalysisKinds
 import PrettyPrinters.BigO
 import System.Directory
 
-prettyPrintAnalysis :: String -> KindOfAnalysis -> IO ()
-prettyPrintAnalysis fileName analysisKind =
+prettyPrintAnalysis :: String -> String -> KindOfAnalysis -> IO ()
+prettyPrintAnalysis hstociaoDir fileName analysisKind =
   case analysisKind of
     NoAnalysis -> return () -- do nothing
     _ ->
       do
-        fileContents <- readFile $ "./out/" ++ fileName ++ oldFileSuffix
+        fileContents <- readFile $ hstociaoDir ++ "/out/" ++ fileName ++ oldFileSuffix
         let prettifiedOutput = prettifier fileContents
         if prettifiedOutput == ""
           then putStrLn "\nWARNING: Since the analysis didn't finish properly, no pretty-printing has been done.\n"
           else do
-            writeFile ("./out/" ++ fileName ++ fileSuffix) $ prettifiedOutput
-            removeFile $ "./out/" ++ fileName ++ oldFileSuffix
+            let resultsFile = hstociaoDir ++ "/out/" ++ fileName ++ fileSuffix
+            writeFile resultsFile prettifiedOutput
+            putStrLn $ "\n----------------------------------"
+            putStrLn $ "\nThe analysis results have been written to " ++ resultsFile
+            removeFile $ hstociaoDir ++ "/out/" ++ fileName ++ oldFileSuffix
   where
     oldFileSuffix = analysisOldFileSuffix analysisKind
     fileSuffix = analysisFileSuffix analysisKind
